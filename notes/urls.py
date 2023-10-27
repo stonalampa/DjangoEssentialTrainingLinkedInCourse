1a5a6cfe39c1205da151344ada849583f2df2e9b
Milan <milan.radanovic@sprintingsoftware.com>
2023-10-28 01:22:34 +0200

    Done

--- notes/urls.py
+++ notes/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('notes', views.NotesListView.as_view()),
     path('notes/<int:pk>', views.NotesDetailView.as_view()),
     path('notes/new', views.NotesCreateView.as_view(), name="notes.new"),
+    path('notes/<int:pk>/edit', views.NotesUpdateView.as_view(), name='notes.update'),
+    path('notes/<int:pk>/delete',
+         views.NotesDeleteView.as_view(), name='notes.delete'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,21 +1,49 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.http import Http404
-from django.views.generic import ListView, DetailView, CreateView
+from django.http import Http404, HttpResponseRedirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from .forms import NotesFrom
 from .models import Notes
+
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes/'
+    template_name = 'notes/notes_delete.html'
+
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes/'
+    form_class = NotesFrom
 
 
 class NotesCreateView(CreateView):
     model = Notes
     success_url = '/smart/notes/'
     form_class = NotesFrom
+    login_url = '/admin'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = 'notes'
     template_name = 'notes/notes_list.html'
+    login_url = '/admin'
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 
 # def list(request):
